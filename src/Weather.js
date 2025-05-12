6d07295b8544581586e47a7c5a066cc3af2d6c1b
Chani Kaufman <ckaufman@by.local>
2025-05-12 15:24:40 +0100

    build settings

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,5 @@ import React from "react";
 import axios from "axios";
 
 export default function Weather() {
-    let apiKey =
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
-    return <h2>Hello from Weather</h2>
+  return <h2>Hello from Weather</h2>;
 }
